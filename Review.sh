8608da278375f4b68feb1ee7ba66e23355534c9f
Rutuja412 <rutujakadam4545@gmail.com>
2022-11-21 08:35:35 +0530

    [Rutuja412]-Adding biggernumber in Review

--- Review.sh
+++ Review.sh
@@ -1,2 +1,10 @@
-#!/bin/bash
-echo welcome review
+#!/bin/bash -x
+read -p "enter 1st number" num1
+read -p "enter 2nd number" num2
+
+if [ $num1 -gt $num2 ]
+then
+ echo $num1
+else
+ echo $num2
+fi
